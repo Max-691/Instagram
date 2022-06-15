8b65f95e586cfc7022eb0fef72943f7110a549b2
Maxime <mcluzel@edenschool.fr>
2022-06-15 10:17:01 +0200

    Finishe DOM, starting new CSS

--- dom/js/app.js
+++ dom/js/app.js
@@ -35,29 +35,52 @@ const postImg = document.createElement('img');
 postImg.src="https://random.imagecdn.app/500/150";
 mainDiv.appendChild(postImg);
 
+
+//Bottom div
 const bottomDiv = document.createElement('div');
 bottomDiv.classList.add('bottom-div');
 article.appendChild(bottomDiv);
 
+//Ul contains like, comment, share...
 const bottomUl = document.createElement('ul');
 bottomDiv.appendChild(bottomUl);
 
+//Like li with img into
 const likedLi = document.createElement('li');
 bottomUl.appendChild(likedLi);
+
+//Like img
 const likedImg = document.createElement('img');
 likedImg.src="../img/like.png";
 likedLi.appendChild(likedImg);
 
+// Comment li with img into
 const commentLi = document.createElement('li');
 bottomUl.appendChild(commentLi);
 
+//Comment img
 const commentImg = document.createElement('img');
 commentImg.src="../img/comment.png";
 commentLi.appendChild(commentImg);
 
+// Share li with img into
 const shareLi = document.createElement('li');
 bottomUl.appendChild(shareLi);
 
+//Share img
 const shareImg = document.createElement('img');
 shareImg.src="../img/send.png";
 shareLi.appendChild(shareImg);
+
+//Number of likes (paragraph)
+const nbOfLikes = document.createComment('p');
+bottomUl.appendChild(nbOfLikes);
+
+
+//Bookmark button at right of likes, comment, share
+const bookmarked = document.createElement('div');
+bottomDiv.appendChild(bookmarked);
+
+const bookmarkedImg = document.createElement('img');
+bookmarkedImg.src="../img/bookmark.png";
+bookmarked.appendChild(bookmarkedImg);
